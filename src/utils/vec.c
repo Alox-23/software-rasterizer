10c114f08875b3813b606858862aa626708c2566
hackerman <akuznetsov23000@gmail.com>
2025-12-05 22:52:37 +0100

    finished writing the function logic of the new vector passed by pointer system that should speed thing up

--- src/utils/vec.c
+++ src/utils/vec.c
@@ -5,151 +5,121 @@ void print_vec4f(char* name, vec4f_t a){
 }
 
 // basic arithmetic
-vec4f_t add_vec4f(vec4f_t a, vec4f_t b){
-  vec4f_t result;
-
+void add_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b){
   for (int i = 0; i < 4; i++){
-    result.v[i] = a.v[i] + b.v[i];
+    r->v[i] = a->v[i] + b->v[i];
   }
- 
-  return result;
 }
 
-vec4f_t sub_vec4f(vec4f_t a, vec4f_t b){
-  vec4f_t result;
-
+void sub_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b){
   for (int i = 0; i < 4; i++){
-    result.v[i] = a.v[i] - b.v[i];
+    r->v[i] = a->v[i] - b->v[i];
   }
- 
-  return result;
 }
 
-vec4f_t mul_vec4f(vec4f_t a, vec4f_t b){
-  vec4f_t result;
-
+void mul_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b){
   for (int i = 0; i < 4; i++){
-    result.v[i] = a.v[i] * b.v[i];
+    r->v[i] = a->v[i] * b->v[i];
   }
-
-  return result;
 }
 
-vec4f_t scl_vec4f(vec4f_t a, float s){
-  vec4f_t result;
-
+void scl_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, float s){
   for (int i = 0; i < 4; i++){
-    result.v[i] = a.v[i] * s;
+    r->v[i] = a->v[i] * s;
   }
-
-  return result;
 }
 
 // complex functions
-vec4f_t neg_vec4f(vec4f_t a){
-  return (vec4f_t){-a.x, -a.y, -a.z, 0};
+void neg_vec4f(vec4f_t* restrict r, vec4f_t* restrict a){
+  for (int i = 0; i < 4; i++){
+    r->v[i] = -a->v[i];
+  }
 }
 
-float dot_vec4f(vec4f_t a, vec4f_t b){
+float dot_vec4f(vec4f_t* restrict a, vec4f_t* restrict b){
   float result = 0;
 
   for (int i = 0; i < 4; i++){
-    result = result + a.v[i] * b.v[i]; // result += a[i] * b[i]
+    result += a->v[i] * b->v[i];
   }
 
   return result;
 }
 
-float mag_sq_vec4f(vec4f_t a){
-   return a.x*a.x + a.y*a.y + a.z*a.z; // ignoire w component
+float mag_sq_vec4f(vec4f_t* restrict a){
+   return a->x*a->x + a->y*a->y + a->z*a->z; // ignoire w component
 }
 
-vec4f_t nrm_vec4f(vec4f_t a){
+vec4f_t nrm_vec4f(vec4f_t* restrict  a){
   float len_sq = mag_sq_vec4f(a);
 
   if (len_sq > 0){
     float inv_len = 1 / sqrt(len_sq);
     return (vec4f_t){
-      a.x * inv_len,
-      a.y * inv_len,
-      a.z * inv_len,
+      a->x * inv_len,
+      a->y * inv_len,
+      a->z * inv_len,
       0 
     };
   }
 }
 
-vec4f_t pdiv_vec4f(vec4f_t v){
-  v.x /= v.w;
-  v.y /= v.w;
-  v.z /= v.w;
-  return v;
+void pdiv_vec4f(vec4f_t* restrict r, vec4f_t* restrict v){
+  float w = v->v[3];
+
+  r->x /= v->x / w;
+  r->y /= v->y / w;
+  r->z /= v->z / w;
+  r->w /= v->w;
 }
 
-float dst_sq_vec4f(vec4f_t a, vec4f_t b){
-  float dx = a.x - b.x;
-  float dy = a.y - b.y;
-  float dz = a.z - b.z;
+float dst_sq_vec4f(vec4f_t* restrict a, vec4f_t* restrict b){
+  float dx = a->x - b->x;
+  float dy = a->y - b->y;
+  float dz = a->z - b->z;
   return dx*dx + dy*dy + dz*dz;
 }
 
-float det2d_vec4f(vec4f_t a, vec4f_t b){
-  return a.x * b.y - a.y * b.x;
+float det2d_vec4f(vec4f_t* restrict a, vec4f_t* restrict b)
+  return a->x * b->y - a->y * b->x;
 } 
 
-bool left_or_top_edge(vec4f_t start, vec4f_t end){
-  vec4f_t edge = sub_vec4f(end, start);
+bool left_or_top_edge(vec4f_t* restrict start, vec4f_t* restrict end){
+  vec4f_t edge = {};
+  sub_vec4f(&edge, end, start);
 
   const bool is_left_edge = edge.y > 0;
   const bool is_top_edge = edge.y == 0 && edge.x < 0;
   return is_left_edge || is_top_edge;
 }
+
 //utility functions
-vec4f_t min_vec4f(vec4f_t a, vec4f_t b){
-  vec4f_t result;
-
+void min_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b){
   for (int i = 0; i < 4; i++){
-    result.v[i] = fmin(a.v[i], b.v[i]);
+    r->v[i] = fmin(a->v[i], b->v[i]);
   }
-
-  return result;
 }
 
-vec4f_t max_vec4f(vec4f_t a, vec4f_t b){
-  vec4f_t result;
-
+void max_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b){
   for (int i = 0; i < 4; i++){
-    result.v[i] = fmax(a.v[i], b.v[i]);
+    r->v[i] = fmax(a->v[i], b->v[i]);
   }
-
-  return result;
 }
 
-vec4f_t flr_vec4f(vec4f_t a){
-  vec4f_t result;
-
+void flr_vec4f(vec4f_t* restrict r, vec4f_t* restrict a){
   for (int i = 0; i < 4; i++){
-    result.v[i] = floor(a.v[i]);
+    r->v[i] = floor(a->v[i]);
   }
-  
-  return result;
 }
 
-vec4f_t cil_vec4f(vec4f_t a){
-  vec4f_t result;
-
+void cil_vec4f(vec4f_t* restrict r, vec4f_t* restrict a){
   for (int i = 0; i < 4; i++){
-    result.v[i] = ceil(a.v[i]);
+    r->v[i] = ceil(a->v[i]);
   }
-  
-  return result;
 }
 
-vec4f_t abs_vec4f(vec4f_t a){
-  vec4f_t result;
-
+void abs_vec4f(vec4f_t* restrict r, vec4f_t* restrict a){
   for (int i = 0; i < 4; i++){
-    result.v[i] = fabs(a.v[i]);
+    r->v[i] = fabs(a->v[i]);
   }
-
-  return result;
 }
--- src/utils/vec.h
+++ src/utils/vec.h
@@ -23,20 +23,20 @@ typedef union{
 void print_vec4f(char* name, vec4f_t a);
 
 //basic arithmetic
-vec4f_t add_vec4f(vec4f_t a, vec4f_t b); // returns a + b
-vec4f_t sub_vec4f(vec4f_t a, vec4f_t b); // returns a - b 
-vec4f_t mul_vec4f(vec4f_t a, vec4f_t b); // returns a * b
-vec4f_t scl_vec4f(vec4f_t a, float s); // returns a * s
+void add_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b); // returns a + b
+void sub_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b); // returns a - b 
+void mul_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, vec4f_t* restrict b); // returns a * b
+void scl_vec4f(vec4f_t* restrict r, vec4f_t* restrict a, float s); // returns a * s
 
 //complex funcitons
-vec4f_t neg_vec4f(vec4f_t a); // returns -a
-float dot_vec4f(vec4f_t a, vec4f_t b); // return dot product of a, b
-float mag_sq_vec4f(vec4f_t a); // ONLY FOR VECTORS, magnitude of a vector squared
-vec4f_t nrm_vec4f(vec4f_t a); // ONLY FOR VECTORS
-vec4f_t pdiv_vec4f(vec4f_t v);
-float dst_sq_vec4f(vec4f_t a, vec4f_t b); // distance between two POINTS squared
-float det2d_vec4f(vec4f_t a, vec4f_t b); // the determinant of the vector along the XY plane (for drawring point-in-triangle test)
-bool left_or_top_edge(vec4f_t start, vec4f_t end); // helps to remove overdraw in cases where triangle edges overlap
+void neg_vec4f(vec4f_t* restrict r, vec4f_t* restrict a); // returns -a
+float dot_vec4f(vec4f_t* restrict a, vec4f_t* restrict b); // return dot product of a, b
+float mag_sq_vec4f(vec4f_t* restricta); // ONLY FOR VECTORS, magnitude of a vector squared
+vec4f_t nrm_vec4f(vec4f_t* restrict a); // ONLY FOR VECTORS
+void pdiv_vec4f(vec4f_t* restrict r, vec4f_t* restrict v);
+float dst_sq_vec4f(vec4f_t* restricta, vec4f_t* restrict b); // distance between two POINTS squared
+float det2d_vec4f(vec4f_t* restrict a, vec4f_t* restrict b); // the determinant of the vector along the XY plane (for drawring point-in-triangle test)
+bool left_or_top_edge(vec4f_t* restrict start, vec4f_t* restrict end); // helps to remove overdraw in cases where triangle edges overlap
 
 //utility functions
 vec4f_t min_vec4f(vec4f_t a, vec4f_t b); // min of a, b
